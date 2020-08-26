d90da821fe9b32afa2f807e688d9acc090646667
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2020-08-26 16:10:40 +0300

    adding sort e2e tests

--- test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Search/SearchTestsBase.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Search/SearchTestsBase.cs
@@ -38,6 +38,15 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
             return await ExecuteAndValidateBundle(searchUrl, searchUrl, sort, expectedResources);
         }
 
+        protected async Task<Bundle> ExecuteAndValidateBundleSuperset(string searchUrl, bool sort, params Resource[] expectedResources)
+        {
+            Bundle bundle = await Client.SearchAsync(searchUrl);
+
+            ValidateBundleIsSuperset(bundle, sort, expectedResources);
+
+            return bundle;
+        }
+
         protected async Task<Bundle> ExecuteAndValidateBundle(string searchUrl, string selfLink, params Resource[] expectedResources)
         {
             Bundle bundle = await Client.SearchAsync(searchUrl);
@@ -112,6 +121,26 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
             Assert.Collection(
                 bundle.Entry.Select(e => e.Resource),
                 expectedResources.Select(er => new Action<Resource>(r => Assert.True(er.IsExactly(r)))).ToArray());
+        }
+
+        protected void ValidateBundleIsSuperset(Bundle bundle, bool sort, params Resource[] expectedResources)
+        {
+            Assert.NotNull(bundle);
+
+            if (sort)
+            {
+                bundle.Entry.Sort((a, b) => string.CompareOrdinal(a.Resource.Id, b.Resource.Id));
+                Array.Sort(expectedResources, (a, b) => string.CompareOrdinal(a.Id, b.Id));
+            }
+
+            // make sure the result is not empty
+            Assert.NotEmpty(bundle.Entry.Select(e => e.Resource));
+
+            // make sure the returned result contains all of the expected item
+            // maybe some more items...
+            Assert.Subset(
+                bundle.Entry.Select(e => e.Resource).ToHashSet(),
+                expectedResources.Select(er => er).ToHashSet());
         }
     }
 }
